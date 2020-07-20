0584733df38886596eae623e85de818eb57746ba
Zeel <zeelp741@gmail.com>
2020-07-20 01:36:13 -0400

    Update MainActivity.java

--- app/src/main/java/com/zeelp/connect3/MainActivity.java
+++ app/src/main/java/com/zeelp/connect3/MainActivity.java
@@ -11,7 +11,6 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
 
     int activePlayer = 0;  // 0 is yellow, 1 is red
-
     boolean gameIsActive = true;
 
     int[] gameState = {2, 2, 2, 2, 2, 2, 2, 2, 2}; // 2 means spot un-played
@@ -23,72 +22,44 @@ public class MainActivity extends AppCompatActivity {
         int tappedCounter = Integer.parseInt(counter.getTag().toString());
 
         if(gameState[tappedCounter]==2 && gameIsActive) {
-
             gameState[tappedCounter] = activePlayer;
-
             counter.setTranslationY(-1000f);
-
             if (activePlayer == 0) {
-
                 counter.setImageResource(R.drawable.yellow);
-
                 activePlayer = 1;
-
             } else {
-
                 counter.setImageResource(R.drawable.red1);
-
                 activePlayer = 0;
             }
             counter.animate().translationYBy(1000f).setDuration(300);
-
 
             for (int[] winningPosition : winningPositions) {
                 if (gameState[winningPosition[0]] == gameState[winningPosition[1]] &&
                         gameState[winningPosition[1]] == gameState[winningPosition[2]] &&
                         gameState[winningPosition[0]] != 2) {
-
                     gameIsActive = false;
-
                     String winner = "Red";
-
                     if (gameState[winningPosition[0]] == 0) {
-
                         winner = "Yellow";
-
                     }
 
                     //someone has won
-
                     TextView playAgainTextView = (TextView) findViewById(R.id.playAgainTextView);
-
                     playAgainTextView.setText(winner + " Has Won");
-
-
                     LinearLayout playAgainLayout = (LinearLayout) findViewById(R.id.playAgainLayout);
-
                     playAgainLayout.setVisibility(View.VISIBLE);
 
                 } else {
                     boolean gameIsOver = true;
-
                     for (int counterState : gameState) {
-
                         if (counterState == 2) gameIsOver = false;
-
                     }
 
-
                     if (gameIsOver) {
-
                         TextView winnerMessage = (TextView) findViewById(R.id.playAgainTextView);
-
                         winnerMessage.setText("It's a draw");
-
                         LinearLayout layout = (LinearLayout) findViewById(R.id.playAgainLayout);
-
                         layout.setVisibility(View.VISIBLE);
-
                     }
                 }
             }
@@ -99,34 +70,22 @@ public class MainActivity extends AppCompatActivity {
 
     public void resetButton(View view) {
         gameIsActive = true;
-
         LinearLayout layout = (LinearLayout) findViewById(R.id.playAgainLayout);
-
         layout.setVisibility(View.INVISIBLE);
-
         activePlayer = 0;
-
         for (int i = 0; i < gameState.length; i++) {
-
             gameState[i] = 2;
-
         }
 
      GridLayout gridLayout = (GridLayout)findViewById(R.id.gridLayout);
-
         for (int i = 0; i< gridLayout.getChildCount(); i++) {
-
             ((ImageView) gridLayout.getChildAt(i)).setImageResource(0);
-
         }
     }
-
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
     }
 }
